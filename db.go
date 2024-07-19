54ad72357a4697993bb46e4609297c6ead606529
YizhakTime <ydcohen@mail.csuchico.edu>
2024-07-19 10:44:24 -0700

    Adding go database files, base html

---
+++ db.go
@@ -0,0 +1,19 @@
+package main
+import (
+	"database/sql"
+	/*
+	"context"
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+	"time"
+	*/
+)
+
+var pool *sql.DB // Database connection pool.
+
+func main() {
+
+
+}
